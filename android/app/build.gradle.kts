4754c4f00143aef4ce78a5a28f27072f4cea1bb4
gyx1231 <2016115954@qq.com>
2026-06-04 12:30:08 +0800

    fix: 修复NDK版本为27.0.12077973适配插件要求

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.yourname.memo_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
